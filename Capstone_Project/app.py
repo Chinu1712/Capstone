ce655be4677db3d961aa9c5d1c2423860e67be4e
Chinmay Gandhewar <134136784+Chinu1712@users.noreply.github.com>
2025-02-23 16:27:24 +0530

    app.py

--- Capstone_Project/app.py
+++ Capstone_Project/app.py
@@ -22,6 +22,7 @@ def predict():
 
 # Run the app
 if __name__ == "__main__":
-    print("Flask app is starting...")
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
+
 
